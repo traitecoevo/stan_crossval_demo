794534238313260b1261493861badff8c7e8fae4
James Camac <james.camac@gmail.com>
2016-04-27 10:59:24 +1000

    Fixed major typos in model code

--- R/model.R
+++ R/model.R
@@ -30,17 +30,21 @@ stan_model <- function() {
     
     # Priors
     alpha ~ normal(0,10);
-    beta ~ normal(0, 10);
+    beta ~ normal(0,10);
     sigma_obs ~ cauchy(0,5);
   }
-  generated quantities{
+  generated quantities {
     real loglik_heldout;
     real total_loglik_heldout;
     real y_hat_heldout[n_obs_heldout];
+
+    # initialize total_loglik_heldout
+    total_loglik_heldout <- 0;
     
     for (i in 1:n_obs_heldout) {
       y_hat_heldout[i] <- alpha + beta * ln_alf_heldout[i];
-      total_loglik_heldout <- total_loglik_heldout + loglik_heldout; 
+      loglik_heldout <- normal_log(ln_ht_heldout[i], y_hat_heldout[i], sigma_obs);
+      total_loglik_heldout <- total_loglik_heldout + loglik_heldout;
       # We are only monitoring the summed loglik here.
       # But above can easily be modified to give obervation log likelihood
       # However, if you are dealing with large datasets this will consume alot of memory.
@@ -53,20 +57,16 @@ stan_model <- function() {
 # Model with random effects
 stan_model_re <- function() {
   list(
-    pars = c('alpha_mu','beta_mu','alpha_sigma','beta_sigma','sigma_spp','sigma_site','sigma_obs','loglik_heldout'),
+    pars = c('alpha_mu','beta_mu','alpha_sigma','beta_sigma','sigma_spp','sigma_obs','loglik_heldout'),
     model = "
     data {
       int<lower = 1> n_obs; 
-      int<lower = 1> n_site;
       int<lower = 1> n_spp;
-      int<lower = 1> site[n_obs]; 
       int<lower = 1> spp[n_obs];
       real ln_alf[n_obs];
       real ln_ht[n_obs];
       int<lower = 1> n_obs_heldout; 
-      int<lower = 1> n_site_heldout;
       int<lower = 1> n_spp_heldout;
-      int<lower = 1> site_heldout[n_obs_heldout]; 
       int<lower = 1> spp_heldout[n_obs_heldout];
       real ln_alf_heldout[n_obs_heldout];
       real ln_ht_heldout[n_obs_heldout];
@@ -80,8 +80,6 @@ stan_model_re <- function() {
     real<lower=0> alpha_sigma;
     real beta_mu;
     real<lower=0> beta_sigma;
-    real site_effect[n_site];
-    real<lower=0> sigma_site;
     real<lower=0> sigma_spp;
     real<lower=0> sigma_obs;
   }
@@ -89,28 +87,29 @@ stan_model_re <- function() {
     real y_hat[n_obs];
     
     for (i in 1:n_obs) {
-      y_hat[i] <- alpha[spp[i]] + beta[spp[i]] * ln_alf[i] + site_effect[site[i]];
+      y_hat[i] <- alpha[spp[i]] + beta[spp[i]] * ln_alf[i];
     }
     ln_ht ~ normal(y_hat, sigma_obs);
     
     # Priors
     alpha ~ normal(alpha_mu,alpha_sigma);
-    beta ~ normal(alpha, 10);
-    alpha_mu ~ normal(0, 10);
-    alpha_sigma ~ cauchy(0, 5);
+    beta ~ normal(beta_mu,10);
+    alpha_mu ~ normal(0,10);
+    alpha_sigma ~ cauchy(0,5);
     beta_mu ~ normal(0, 10);
     beta_sigma ~ cauchy(0,5);
-    site_effect ~ normal(0,sigma_site);
-    sigma_site ~ cauchy(0,5);
     sigma_obs ~ cauchy(0,5);
   }
-  generated quantities{
+  generated quantities {
     real loglik_heldout;
     real total_loglik_heldout;
     real y_hat_heldout[n_obs_heldout];
+
+    # initialize total_loglik_heldout
+    total_loglik_heldout <- 0;
     
     for (i in 1:n_obs_heldout) {
-      y_hat_heldout[i] <- alpha[spp_heldout[i]] + beta[spp_heldout[i]] * ln_alf_heldout[i] + site_effect[site_heldout[i]];
+      y_hat_heldout[i] <- alpha[spp_heldout[i]] + beta[spp_heldout[i]] * ln_alf_heldout[i];
       loglik_heldout <- normal_log(ln_ht_heldout[i], y_hat_heldout[i], sigma_obs);
       total_loglik_heldout <- total_loglik_heldout + loglik_heldout;
       # We are only monitoring the summed loglik here.
