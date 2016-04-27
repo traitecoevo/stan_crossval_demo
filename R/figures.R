e4e5fc6bef30c3e050f89099f9584d32f99b0168
James Camac <james.camac@gmail.com>
2016-04-27 14:40:03 +1000

    functions to produce figure

---
+++ R/figures.R
@@ -0,0 +1,27 @@
+# Plot theme
+partial_plot_theme <- function(legend.position = "none", strips = FALSE,...) {
+  sb <- if(strips==TRUE) element_rect(fill='lightgrey') else element_blank()
+  st <- if(strips==TRUE) element_text(face='italic') else element_blank()
+  theme_classic(base_size = 7) + theme(strip.text = st,
+                          legend.title = element_blank(),
+                          strip.background = sb,
+                          legend.position = legend.position,
+                          axis.line.x = element_line(colour = 'black', size=0.5, linetype='solid'),
+                          axis.line.y = element_line(colour = 'black', size=0.5, linetype='solid'),
+                          plot.margin = unit(c(3,3,3,3), "mm"))
+}
+
+# Plot log likelhoods for each model
+plot_loglik <- function(loglik_summary) {
+  dat <- loglik_summary %>%
+  mutate(model = factor(comparison, levels=c("without_random_effects","with_random_effects"))) %>%
+  mutate(model = factor(comparison, labels = c( "model","model + species effects"))) %>%
+  arrange(comparison)
+  
+  ggplot(dat, aes(x = model,y = mean)) + 
+  geom_pointrange(aes(ymin = `2.5%`, ymax=`97.5%`), position=position_dodge(.1), size=0.2) +
+  ylab('Log likelihood') + 
+  xlab('Model') +
+  scale_y_continuous(breaks=scales::pretty_breaks(4)) +
+  partial_plot_theme()
+}
